2e0aa0da087558bee2150fff4189bb208ccf7f5c
Josef Pihrt <josef.pihrt@outlook.com>
2015-05-16 13:50:48 +0200

    CharClassGroup refactored

--- src/Regexator/Builder/CharGroupExpression/CharClassGroup.cs
+++ src/Regexator/Builder/CharGroupExpression/CharClassGroup.cs
@@ -1,14 +1,10 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-
 namespace Pihrtsoft.Regexator.Builder
 {
     internal class CharClassGroup
         : CharGroupExpression
     {
-        private readonly IEnumerable<CharClass> _values;
         private readonly CharClass _value;
 
         public CharClassGroup(CharClass value)
@@ -16,28 +12,9 @@ namespace Pihrtsoft.Regexator.Builder
             _value = value;
         }
 
-        public CharClassGroup(IEnumerable<CharClass> values)
-        {
-            if (values == null)
-            {
-                throw new ArgumentNullException("values");
-            }
-            _values = values;
-        }
-
         public override string Content
         {
-            get
-            {
-                if (_values != null)
-                {
-                    return Syntax.CharClasses(_values);
-                }
-                else
-                {
-                    return Syntax.CharClass(_value);
-                }
-            }
+            get { return Syntax.CharClass(_value); }
         }
     }
 }
