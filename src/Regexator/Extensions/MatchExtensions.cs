3ac0e83d1598ee787cfcc7d4c6d1ffda49c4859b
Josef Pihrt <josef.pihrt@outlook.com>
2015-06-02 19:45:53 +0200

    MatchExtensions class added

---
+++ src/Regexator/Extensions/MatchExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Pihrtsoft.Text.RegularExpressions.Extensions
+{
+    public static class MatchExtensions
+    {
+        public static Group Group(this Match match, string groupName)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException("match");
+            }
+
+            return match.Groups[groupName];
+        }   
+    }
+}
