421319100a4bc63843e8169173690995718a63e2
Josef Pihrt <josef.pihrt@outlook.com>
2015-06-14 22:00:53 +0200

    checks added

--- src/Regexator/Linq/CharGroupItem/RangeCharItem.cs
+++ src/Regexator/Linq/CharGroupItem/RangeCharItem.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+
 namespace Pihrtsoft.Text.RegularExpressions.Linq
 {
     internal sealed class RangeCharItem
@@ -10,6 +12,11 @@ namespace Pihrtsoft.Text.RegularExpressions.Linq
 
         public RangeCharItem(char firstChar, char lastChar)
         {
+            if (lastChar < firstChar)
+            {
+                throw new ArgumentOutOfRangeException("lastChar");
+            }
+
             _firstChar = firstChar;
             _lastChar = lastChar;
         }
