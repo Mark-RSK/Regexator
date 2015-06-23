07cb7f77eb4da6cacdc34e2c164ff14f5c7319d9
Josef Pihrt <josef.pihrt@outlook.com>
2015-06-23 23:38:47 +0200

    file renamed

--- src/Regexator/Linq/Pattern.cs
+++ src/Regexator/Linq/Pattern.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Linq;
 using System.Text.RegularExpressions;
 using Pihrtsoft.Text.RegularExpressions.Extensions;
 
