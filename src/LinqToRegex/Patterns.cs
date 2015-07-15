80c5ca264dec54a6e93103920fe5ee03a1e739ab
Josef Pihrt <josef.pihrt@outlook.com>
2015-07-15 20:25:07 +0200

    Balance method reformatted

--- src/LinqToRegex/Patterns.cs
+++ src/LinqToRegex/Patterns.cs
@@ -5307,8 +5307,12 @@ namespace Pihrtsoft.Text.RegularExpressions.Linq
 
         internal static Pattern Balance(char openChar, char closeChar, string openGroupName, string closeGroupName)
         {
-            return OneMany(NamedGroup(openGroupName, openChar).WhileNotChar(openChar, closeChar))
-                .OneMany(BalancingGroup(closeGroupName, openGroupName, Character(closeChar)).WhileNotChar(openChar, closeChar));
+            return OneMany(
+                    NamedGroup(openGroupName, openChar)
+                    .WhileNotChar(openChar, closeChar))
+                .OneMany(
+                    BalancingGroup(closeGroupName, openGroupName, Character(closeChar))
+                    .WhileNotChar(openChar, closeChar));
         }
 #endif
     }
